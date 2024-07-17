08530c801b737f829d7b3a452a96dc7bea364dba
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-17 16:07:03 +0530

    refactoring code

--- server.c
+++ server.c
@@ -2,8 +2,18 @@
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+struct AcceptedSocket {
+  int acceptedSocketFD;
+  struct sockaddr_in address;
+  int error;
+  bool acceptedSuccessfully;
+};
+
+struct AcceptedSocket *acceptIncomingConnection(int serverSocketFD);
 
 int main() {
   int serverSocketFD = createTCPIpv4Socket();
@@ -16,14 +26,13 @@ int main() {
 
   int listenResult = listen(serverSocketFD, 10);
 
-  struct sockaddr_in clientAddress;
-  int clientAddressSize = sizeof(struct sockaddr_in);
-  int clientSocketFD = accept(serverSocketFD, (struct sockaddr *)&clientAddress,
-                              (socklen_t *)&clientAddressSize);
+  struct AcceptedSocket *clientSocket =
+      acceptIncomingConnection(serverSocketFD);
 
   char buffer[1024];
   while (true) {
-    ssize_t amountRecieved = recv(clientSocketFD, buffer, 1024, 0);
+    ssize_t amountRecieved =
+        recv(clientSocket->acceptedSocketFD, buffer, 1024, 0);
     if (amountRecieved > 0) {
       buffer[amountRecieved] = 0;
       printf("Response from client: %s\n", buffer);
@@ -33,8 +42,26 @@ int main() {
       break;
     }
   }
-  close(clientSocketFD);
+  close(clientSocket->acceptedSocketFD);
   shutdown(serverSocketFD, SHUT_RDWR);
 
   return 0;
+}
+
+struct AcceptedSocket *acceptIncomingConnection(int serverSocketFD) {
+  struct sockaddr_in clientAddress;
+  int clientAddressSize = sizeof(struct sockaddr_in);
+  int clientSocketFD = accept(serverSocketFD, (struct sockaddr *)&clientAddress,
+                              (socklen_t *)&clientAddressSize);
+
+  struct AcceptedSocket *acceptedSocket = malloc(sizeof(struct AcceptedSocket));
+  acceptedSocket->address = clientAddress;
+  acceptedSocket->acceptedSocketFD = clientSocketFD;
+  acceptedSocket->acceptedSuccessfully = clientSocketFD > 0;
+
+  if (!acceptedSocket->acceptedSuccessfully) {
+    acceptedSocket->error = clientSocketFD;
+  }
+
+  return acceptedSocket;
 }
